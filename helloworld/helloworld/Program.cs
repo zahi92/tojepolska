82ab619e9f38bc49d8a8f0da45c3cb485948b0fd
Damian <damian.zahi@gmail.com>
2018-03-09 12:11:30 +0100

    dodanie programu hello world do repo

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
